a186fde1ddc75e8c1aa88e6c4a3a3fec760bbea9
Jão Xavier <jvxavier.lima67@gmail.com>
2023-01-20 09:03:55 -0300

    Controllers with Post and GetById

--- back-end/todoapp/src/main/java/io/github/jaoxavier/todoapp/rest/controller/ProjectController.java
+++ back-end/todoapp/src/main/java/io/github/jaoxavier/todoapp/rest/controller/ProjectController.java
@@ -1,10 +1,29 @@
 package io.github.jaoxavier.todoapp.rest.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.jaoxavier.todoapp.domain.entity.Project;
+import io.github.jaoxavier.todoapp.domain.repository.ProjectRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/projects/")
 public class ProjectController {
+    private final ProjectRepository projectRepository;
+
+    @PostMapping("new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Project createNewProject(@RequestBody Project project){
+        return projectRepository.save(project);
+    }
+
+    @GetMapping("idProject/{id}")
+    public Project getProjectById(@PathVariable Integer id){
+        return projectRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Project not found by idProject")
+        );
+    }
 }
--- back-end/todoapp/src/main/java/io/github/jaoxavier/todoapp/rest/controller/TaskController.java
+++ back-end/todoapp/src/main/java/io/github/jaoxavier/todoapp/rest/controller/TaskController.java
@@ -1,10 +1,30 @@
 package io.github.jaoxavier.todoapp.rest.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.github.jaoxavier.todoapp.domain.entity.Task;
+import io.github.jaoxavier.todoapp.domain.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/tasks/")
 public class TaskController {
+
+    private final TaskRepository taskRepository;
+
+    @PostMapping("new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Task createNewTask(@RequestBody Task task){
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("idTask/{id}")
+    public Task getTaskById(@PathVariable Integer id){
+        return taskRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Task not found by idTask")
+        );
+    }
+
 }
