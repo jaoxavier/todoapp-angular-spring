ed524870a08c44b415f3b4ee458eeff908a422a6
Jão Xavier <jvxavier.lima67@gmail.com>
2023-01-20 13:56:48 -0300

    Front Start

--- back-end/todoapp/src/main/java/io/github/jaoxavier/todoapp/rest/controller/ProjectController.java
+++ back-end/todoapp/src/main/java/io/github/jaoxavier/todoapp/rest/controller/ProjectController.java
@@ -7,8 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/projects/")
